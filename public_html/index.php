25eced714c2dafa2ace70b5588b38848831bf029
Greg Bloom <gbloom3@cnm.edu>
2016-10-17 10:31:08 -0600

    Modified index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,43 @@
 <!DOCTYPE html>
-<html lang="eng">
+<html lang="en">
 	<head>
 		<meta charset="utf-8">
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css" />
+		<title>About Me</title>
 	</head>
+	<body>
+		<header>
+			<h1>Hi! I'm Greg. Welcome to my page!</h1>
+			<hr/>
+		</header>
+		<section>
+			<img src="img/me.jpg" alt="This is a picture of me"/>
+			<p>This is the only image I have at my disposal.</p>
+		</section>
+		<section>
+			<h2>My Favorite Things</h2>
+			<ul>
+				<li>Computers</li>
+				<li>Cameras</li>
+				<li>Programming</li>
+				<li>Donuts</li>
+				<li>Sending "Meow" to Sen. Arlo</li>
+			</ul>
+		</section>
+		<section>
+			<h3>My Favorite Activities</h3>
+			<ul>
+				<li>Gaming</li>
+				<li>Photography</li>
+				<li>Writing Stories</li>
+				<li>Cooking</li>
+		</section>
+		<section>
+			<img src="img/bee.jpg" alt="Bee on a flower"/>
+			<img src="img/canyon.jpg" alt="Grand Canyon at dawn"/>
+			<img src="img/dragon.jpg" alt="David looking at dragon"/>
+			<img src="img/half-rainbow.jpg" alt="Half a rainbow"/>
+			<img src="img/truff.jpg" alt="Truffles"/>
+		</section>
+	</body>
 </html>
